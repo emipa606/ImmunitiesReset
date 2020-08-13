f1fca8799de9ddc858fe4bcd71636e64449f8335
emipa606 <emil.offermo@litet.net>
2020-08-13 10:38:14 +0200

    First publish

---
+++ Source/ImmuCorrect/HarmonySetup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace ImmuCorrect
+{
+	// Token: 0x02000004 RID: 4
+	[StaticConstructorOnStartup]
+	public static class HarmonySetup
+	{
+		// Token: 0x06000006 RID: 6 RVA: 0x00002238 File Offset: 0x00000438
+		static HarmonySetup()
+		{
+			new Harmony("ImmuCorrect.Pelador").PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
---
+++ Source/ImmuCorrect/NonPublicFields.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Verse;
+
+namespace ImmuCorrect
+{
+	// Token: 0x02000005 RID: 5
+	[StaticConstructorOnStartup]
+	public static class NonPublicFields
+	{
+		// Token: 0x04000002 RID: 2
+		public static AccessTools.FieldRef<ImmunityHandler, List<ImmunityRecord>> ImmunityList = AccessTools.FieldRefAccess<ImmunityHandler, List<ImmunityRecord>>("immunityList");
+	}
+}
