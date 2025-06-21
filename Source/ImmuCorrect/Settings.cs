7e5ad6274dcf2d6b17bac754f23dd9539e3b3024
Mlie <emipa606@users.noreply.github.com>
2025-06-21 20:51:50 +0200

    Mod updated for 1.6 and passed autotests

--- Source/ImmuCorrect/Settings.cs
+++ Source/ImmuCorrect/Settings.cs
@@ -11,36 +11,36 @@ public class Settings : ModSettings
     public void DoWindowContents(Rect canvas)
     {
         string label = "ImmuCorrect.Reset".Translate();
-        var num = 10f;
-        var listing_Standard = new Listing_Standard { ColumnWidth = canvas.width };
-        listing_Standard.Begin(canvas);
-        listing_Standard.Gap(num);
-        listing_Standard.CheckboxLabeled("ImmuCorrect.AllowReset".Translate(), ref AllowReset);
-        listing_Standard.Gap(num);
-        var posX = 0f;
-        var posY = num * 4f;
-        if (Widgets.ButtonText(GetResetRect(40f, canvas.width / 4f, posX, posY), label, true, false))
+        const float num = 10f;
+        var listingStandard = new Listing_Standard { ColumnWidth = canvas.width };
+        listingStandard.Begin(canvas);
+        listingStandard.Gap(num);
+        listingStandard.CheckboxLabeled("ImmuCorrect.AllowReset".Translate(), ref AllowReset);
+        listingStandard.Gap(num);
+        const float posX = 0f;
+        const float posY = num * 4f;
+        if (Widgets.ButtonText(getResetRect(40f, canvas.width / 4f, posX, posY), label, true, false))
         {
             var list = new List<FloatMenuOption>
             {
-                new FloatMenuOption(label, ImmuCorrection.Correct_missing_immu)
+                new(label, ImmuCorrection.Correct_missing_immu)
             };
             Find.WindowStack.Add(new FloatMenu(list));
         }
 
         if (Controller.currentVersion != null)
         {
-            listing_Standard.Gap(50f);
+            listingStandard.Gap(50f);
             GUI.contentColor = Color.gray;
-            listing_Standard.Label("ImmuCorrect.Version".Translate(Controller.currentVersion));
+            listingStandard.Label("ImmuCorrect.Version".Translate(Controller.currentVersion));
             GUI.contentColor = Color.white;
         }
 
-        listing_Standard.Gap(num);
-        listing_Standard.End();
+        listingStandard.Gap(num);
+        listingStandard.End();
     }
 
-    public static Rect GetResetRect(float height, float width, float posX, float posY)
+    private static Rect getResetRect(float height, float width, float posX, float posY)
     {
         return new Rect(posX, posY, width, height);
     }
